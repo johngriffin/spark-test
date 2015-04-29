557697583c61cef2d8f17b4f85f80af758f0c511
John Griffin <john@atchai.com>
2015-04-29 16:36:01 +0100

    Set absolute path for input file, necessary when running on EC2

--- wordcount.py
+++ wordcount.py
@@ -4,10 +4,10 @@ if __name__ == "__main__":
     conf = SparkConf().setAppName("Test")
     sc = SparkContext(conf=conf)
     
-    file = sc.textFile("pg11.txt")
-    
+    file = sc.textFile("file:///root/spark-test/pg11.txt")
+
     counts = file.flatMap(lambda line: line.split(" ")) \
     .map(lambda word: (word, 1)) \
     .reduceByKey(lambda a, b: a + b)
-    
+
     counts.saveAsTextFile("output")
